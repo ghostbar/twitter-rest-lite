40a10d4b47fafc6183facf3dd7f0459afc18758c
Jose Luis Rivas <me@ghostbar.co>
2014-07-25 02:07:18 -0500

    example/server: small refactor

--- example/server.js
+++ example/server.js
@@ -1,10 +1,17 @@
-var express = require('express'),
-    app = express(),
-    token = {};
+'use strict';
+
+var express = require('express');
+var app = express();
+var token = {};
+
+function gotAccess (res, err, response) {
+  console.log(response);
+  res.send(response);
+}
 
 app.get('/', function(req, res) {
-  res.send("<a href='/auth/twitter'>Sign In with Twitter</a><br>"+
-          "<a href='/apps/twitter'>Authorize with Twitter</a>");
+  res.send('<a href=\'/auth/twitter\'>Sign In with Twitter</a><br>' +
+          '<a href=\'/apps/twitter\'>Authorize with Twitter</a>');
 });
 
 app.get('/auth/twitter', function(req, res) {
@@ -21,20 +28,20 @@ app.get('/auth/twitter', function(req, res) {
     token['token'] = response.oauth_token;
     token['token_secret'] = response.oauth_token_secret;
     oauth.authenticate(token.token, authenticate);
-  };
+  }
 
   function authenticate(err, response) {
     console.log(response);
     res.redirect(response);
-  };
+  }
 });
 
 app.get('/apps/twitter', function(req, res) {
-  var Twitter= require('./../index'),
-      config = require('./config.json'),
-      oauth;
+  var Twitter= require('./../index');
+  var config = require('./config.json');
+  var oauth;
 
-  config['callback'] = 'http://localhost:9999/twitter/authorize';
+  config.callback = 'http://localhost:9999/twitter/authorize';
 
   oauth = new Twitter.OAuth(config);
   oauth.requestToken(requestToken);
@@ -43,37 +50,32 @@ app.get('/apps/twitter', function(req, res) {
     token['token'] = response.oauth_token;
     token['token_secret'] = response.oauth_token_secret;
     oauth.authorize(token.token, authenticate);
-  };
+  }
 
   function authenticate(err, response) {
     console.log(response);
     res.redirect(response);
-  };
+  }
 });
 
 app.get('/twitter/authenticate', function(req, res) {
   if(req.query.oauth_token !== token.token) {
     console.log(req.query.oauth_token);
     console.log(token.token);
-    var err =  new Error("Token is not the same!");
+    var err =  new Error('Token is not the same!');
     res.send(err);
     throw err;
     return;
   }
 
-  console.log("token " + req.query.oauth_token);
-  console.log("verifier " + req.query.oauth_verifier);
+  console.log('token ' + req.query.oauth_token);
+  console.log('verifier ' + req.query.oauth_verifier);
 
-  var Twitter = require('./../index'),
-      config = require('./config.json'),
-      oauth = new Twitter.OAuth(config);
-
-  oauth.accessToken(req.query.oauth_token, req.query.oauth_verifier, gotAccess);
-
-  function gotAccess(err, response) {
-    console.log(response);
-    res.send(response);
-  };
+  var Twitter = require('./../index');
+  var config = require('./config.json');
+  var oauth = new Twitter.OAuth(config);
+  
+  oauth.accessToken(req.query.oauth_token, req.query.oauth_verifier, gotAccess.bind(this, res));
 
 });
 
@@ -81,26 +83,20 @@ app.get('/twitter/authorize', function(req, res) {
   if (req.query.oauth_token !== token.token) {
     console.log(req.query.oauth_token);
     console.log(token.token);
-    var err = new Error("Token is not the same!");
+    var err = new Error('Token is not the same!');
     res.send(err);
     throw err;
-    return;
   }
 
-  console.log("token " + req.query.oauth_token);
-  console.log("verifier " + req.query.oauth_verifier);
+  console.log('token ' + req.query.oauth_token);
+  console.log('verifier ' + req.query.oauth_verifier);
 
-  var Twitter = require('./../index'),
-      config = require('./config.json'),
-      oauth = new Twitter.OAuth(config);
+  var Twitter = require('./../index');
+  var config = require('./config.json');
+  var oauth = new Twitter.OAuth(config);
 
-  oauth.accessToken(req.query.oauth_token, req.query.oauth_verifier, gotAccess);
-
-  function gotAccess(err, response) {
-    console.log(response);
-    res.send(response);
-  };
+  oauth.accessToken(req.query.oauth_token, req.query.oauth_verifier, gotAccess.bind(this, res));
 });
 
 app.listen(9999);
-console.log('Listening on port 9999')
+console.log('Listening on port 9999');
